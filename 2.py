2399292aa51dff755516bdeb9561a39c44613027
Илья <rismiass@yandex.ru>
2020-11-25 11:53:31 +0500

    Решение третьей классной задачи

--- 2.py
+++ 2.py
@@ -1,14 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
-import random
+from ui import Ui_MainWindow
+from random import randint
 from PyQt5.QtGui import QPainter, QColor
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(Ui_MainWindow)
         self.pushButton.clicked.connect(self.run)
         self.flag = 0
 
@@ -19,8 +19,8 @@ class MyWidget(QMainWindow):
         if self.flag == 1:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 215, 0))
-            qp.drawEllipse(200, 200, random.randint(10, 100),  random.randint(10, 100))
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+            qp.drawEllipse(200, 200, randint(10, 100), randint(10, 100))
             qp.end()
 
 
